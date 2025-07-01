80375b3b33a389b1c501ccd211590301bae4681f
Thando Moeketsi Ndlovu <thandomndlovu@gmail.com>
2025-07-01 02:12:13 +0200

    updates

--- Extensions/Newtonsoft/Json/Linq/JObjectExtensions.cs
+++ Extensions/Newtonsoft/Json/Linq/JObjectExtensions.cs
@@ -1,161 +1,161 @@
-﻿using XycloneDesigns.Apis.General.Tables;
+﻿using SQLite;
+
+using System;
+using System.Linq;
+
+using XycloneDesigns.Apis.General.Tables;
 
 namespace Newtonsoft.Json.Linq
 {
 	public static class JObjectExtensions 
 	{
-		public static Country ToCountry(this JObject jobject, StreamWriter logger)
+		public class Params
 		{
-			Country country = new();
-
-			if (jobject.TryGetValue(Country.SQL.Column_Capital, StringComparison.OrdinalIgnoreCase, out JToken? capital) && capital?.ToObject<string?>() is string _capital)
-				country.Capital = _capital;
-			else logger.WriteLine("Capital");
-
-			if (jobject.TryGetValue(Country.SQL.Column_Code, StringComparison.OrdinalIgnoreCase, out JToken? code) && code?.ToObject<string?>() is string _code)
-				country.Code = _code;
-			else logger.WriteLine("Code");
-
-			if (jobject.TryGetValue(Country.SQL.Column_Name, StringComparison.OrdinalIgnoreCase, out JToken? name) && name?.ToObject<string?>() is string _name)
-				country.Name = _name;
-			else logger.WriteLine("Name");
-
-			if (jobject.TryGetValue(Country.SQL.Column_Population, StringComparison.OrdinalIgnoreCase, out JToken? population) && population?.ToObject<int?>() is int _population)
-				country.Population = _population;
-			else logger.WriteLine("Population");
-
-			if (jobject.TryGetValue(Country.SQL.Column_SquareKms, StringComparison.OrdinalIgnoreCase, out JToken? squarekms) && squarekms?.ToObject<decimal?>() is decimal _squarekms)
-				country.SquareKms = _squarekms;
-			else logger.WriteLine("SquareKms");
-
-			if (jobject.TryGetValue(Country.SQL.Column_UrlCoatOfArms, StringComparison.OrdinalIgnoreCase, out JToken? urlcoatofarms) && urlcoatofarms?.ToObject<string?>() is string _urlcoatofarms)
-				country.UrlCoatOfArms = _urlcoatofarms;
-			else logger.WriteLine("UrlCoatOfArms");
-
-			if (jobject.TryGetValue(Country.SQL.Column_UrlWebsite, StringComparison.OrdinalIgnoreCase, out JToken? urlwebsite) && urlwebsite?.ToObject<string?>() is string _urlwebsite)
-				country.UrlWebsite = _urlwebsite;
-			else logger.WriteLine("UrlWebsite");
-
-			return country;
+			public SQLiteConnection? ConnectionCountries { get; set; }
+			public SQLiteConnection? ConnectionDistricts { get; set; }
+			public SQLiteConnection? ConnectionLanguages { get; set; }
+			public SQLiteConnection? ConnectionMunicipalities { get; set; }
+			public SQLiteConnection? ConnectionProvinces { get; set; }
 		}
-		public static Language ToLanguage(this JObject jobject, StreamWriter logger)
+
+		public static Country CountryFromDefault(this JObject jobject, Params? _params)
 		{
-			Language language = new ();
-
-			if (jobject.TryGetValue(Country.SQL.Column_Code, StringComparison.OrdinalIgnoreCase, out JToken? code) && code?.ToObject<string?>() is string _code)
-				language.Code = _code;
-			else logger.WriteLine("Code");
-
-			if (jobject.TryGetValue(Country.SQL.Column_Name, StringComparison.OrdinalIgnoreCase, out JToken? name) && name?.ToObject<string?>() is string _name)
-				language.Name = _name;
-			else logger.WriteLine("Name");
-
-			return language;
+			return new Country
+			{
+				Capital = jobject.GetValue(Country.SQL.Column_Capital, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				Code = jobject.GetValue(Country.SQL.Column_Code, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				Name = jobject.GetValue(Country.SQL.Column_Name, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				Population = jobject.GetValue(Country.SQL.Column_Population, StringComparison.OrdinalIgnoreCase)?.ToObject<int?>(),
+				SquareKms = jobject.GetValue(Country.SQL.Column_SquareKms, StringComparison.OrdinalIgnoreCase)?.ToObject<decimal?>(),
+				UrlCoatOfArms = jobject.GetValue(Country.SQL.Column_UrlCoatOfArms, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				UrlWebsite = jobject.GetValue(Country.SQL.Column_UrlWebsite, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+			};
 		}
-		public static Municipality ToMunicipality(this JObject jobject, StreamWriter logger)
+		public static District? DistrictFromGeography(this JObject jobject, Params? _params)
 		{
-			Municipality municipality = new ();
+			if (jobject.GetValue("parent_level")?.ToObject<string?>() is not string parentlevel || parentlevel != "district")
+				return null;
 
-			if (jobject.TryGetValue(Municipality.SQL.Column_AddressEmail, StringComparison.OrdinalIgnoreCase, out JToken? addressemail) && addressemail?.ToObject<string?>() is string _addressemail)
-				municipality.AddressEmail = _addressemail;
-			else logger.WriteLine("AddressEmail");
+			Country? country = _params?.ConnectionCountries?
+				.Table<Country>()
+				.FirstOrDefault(_ => _.Code == Country.Codes.SouthAfrica);
 
-			if (jobject.TryGetValue(Municipality.SQL.Column_AddressPostal, StringComparison.OrdinalIgnoreCase, out JToken? addresspostal) && addresspostal?.ToObject<string?>() is string _addresspostal)
-				municipality.AddressPostal = _addresspostal;
-			else logger.WriteLine("AddressPostal");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_AddressStreet, StringComparison.OrdinalIgnoreCase, out JToken? addressstreet) && addressstreet?.ToObject<string?>() is string _addressstreet)
-				municipality.AddressStreet = _addressstreet;
-			else logger.WriteLine("AddressStreet");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_Category, StringComparison.OrdinalIgnoreCase, out JToken? category) && category?.ToObject<string?>() is string _category)
-				municipality.Category = _category;
-			else logger.WriteLine("Category");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_GeoCode, StringComparison.OrdinalIgnoreCase, out JToken? geocode) && geocode?.ToObject<string?>() is string _geocode)
-				municipality.GeoCode = _geocode;
-			else logger.WriteLine("GeoCode");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_GeoLevel, StringComparison.OrdinalIgnoreCase, out JToken? geolevel) && geolevel?.ToObject<string?>() is string _geolevel)
-				municipality.GeoLevel = _geolevel;
-			else logger.WriteLine("GeoLevel");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_IsDisestablished, StringComparison.OrdinalIgnoreCase, out JToken? isdisestablished) && isdisestablished?.ToObject<bool?>() is bool _isdisestablished)
-				municipality.IsDisestablished = _isdisestablished;
-			else logger.WriteLine("IsDisestablished");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_MiifCategory, StringComparison.OrdinalIgnoreCase, out JToken? miifcategory) && miifcategory?.ToObject<string?>() is string _miifcategory)
-				municipality.MiifCategory = _miifcategory;
-			else logger.WriteLine("MiifCategory");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_Name, StringComparison.OrdinalIgnoreCase, out JToken? name) && name?.ToObject<string?>() is string _name)
-				municipality.Name = _name;
-			else logger.WriteLine("Name");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_NameLong, StringComparison.OrdinalIgnoreCase, out JToken? namelong) && namelong?.ToObject<string?>() is string _namelong)
-				municipality.NameLong = _namelong;
-			else logger.WriteLine("NameLong");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_NumberFax, StringComparison.OrdinalIgnoreCase, out JToken? numberfax) && numberfax?.ToObject<string?>() is string _numberfax)
-				municipality.NumberFax = _numberfax;
-			else logger.WriteLine("NumberFax");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_NumberPhone, StringComparison.OrdinalIgnoreCase, out JToken? numberphone) && numberphone?.ToObject<string?>() is string _numberphone)
-				municipality.NumberPhone = _numberphone;
-			else logger.WriteLine("NumberPhone");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_Population, StringComparison.OrdinalIgnoreCase, out JToken? population) && population?.ToObject<int?>() is int _population)
-				municipality.Population = _population;
-			else logger.WriteLine("Population");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_SquareKms, StringComparison.OrdinalIgnoreCase, out JToken? squarekms) && squarekms?.ToObject<decimal?>() is decimal _squarekms)
-				municipality.SquareKms = _squarekms;
-			else logger.WriteLine("SquareKms");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_UrlLogo, StringComparison.OrdinalIgnoreCase, out JToken? urllogo) && urllogo?.ToObject<string?>() is string _urllogo)
-				municipality.UrlLogo = _urllogo;
-			else logger.WriteLine("UrlLogo");
-
-			if (jobject.TryGetValue(Municipality.SQL.Column_UrlWebsite, StringComparison.OrdinalIgnoreCase, out JToken? urlwebsite) && urlwebsite?.ToObject<string?>() is string _urlwebsite)
-				municipality.UrlWebsite = _urlwebsite;
-			else logger.WriteLine("UrlWebsite");
-
-
-			return municipality;
+			return new District
+			{
+				Code = jobject.GetValue("parent_code")?.ToObject<string?>(),
+				Name = jobject.GetValue("name")?.ToObject<string?>(),
+				PkCountry = country?.Pk,
+			};
 		}
-		public static Province ToProvince(this JObject jobject, StreamWriter logger)
+		public static Language LanguageFromDefault(this JObject jobject, Params? _params)
 		{
-			Province province = new ();
+			return new Language
+			{
+				Code = jobject.GetValue(Language.SQL.Column_Code, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				Name = jobject.GetValue(Language.SQL.Column_Name, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+			};
+		}
+		public static Municipality MuniciplityFromGeography(this JObject jobject, Params? _params)
+		{
+			Country? country = _params?.ConnectionCountries?
+				.Table<Country>()
+				.FirstOrDefault(_ => _.Code == Country.Codes.SouthAfrica);
 
-			if (jobject.TryGetValue(Province.SQL.Column_Capital, StringComparison.OrdinalIgnoreCase, out JToken? capital) && capital?.ToObject<string?>() is string _capital)
-				province.Capital = _capital;
-			else logger.WriteLine("Capital");
+			District? district = jobject.GetValue("parent_level")?.ToObject<string?>() is not string parentlevel || parentlevel != "district" 
+				? null
+				: _params?.ConnectionDistricts?
+					.TableEnumerable<District>()
+					.FirstOrDefault(_ =>
+					{
+						return string.Equals(_.Code, jobject.GetValue("parent_code")?.ToObject<string?>(), StringComparison.OrdinalIgnoreCase);
+					});
 
-			if (jobject.TryGetValue(Province.SQL.Column_Id, StringComparison.OrdinalIgnoreCase, out JToken? id) && id?.ToObject<string?>() is string _id)
-				province.Id = _id;
-			else logger.WriteLine("Id");
+			Province? province = _params?.ConnectionProvinces?
+				.TableEnumerable<Province>()
+				.FirstOrDefault(_ =>
+				{
+					return
+						string.Equals(_.Code, jobject.GetValue("province_code")?.ToObject<string?>(), StringComparison.OrdinalIgnoreCase) ||
+						string.Equals(_.Name, jobject.GetValue("province_name")?.ToObject<string?>(), StringComparison.OrdinalIgnoreCase);
+				});
 
-			if (jobject.TryGetValue(Province.SQL.Column_Name, StringComparison.OrdinalIgnoreCase, out JToken? name) && name?.ToObject<string?>() is string _name)
-				province.Name = _name;
-			else logger.WriteLine("Name");
+			return new Municipality
+			{
+				AddressPostal = string.Format("{0}\n{1}\n{2}",
+					jobject.GetValue("postal_address_1")?.ToObject<string?>(),
+					jobject.GetValue("postal_address_2")?.ToObject<string?>(),
+					jobject.GetValue("postal_address_3")?.ToObject<string?>()),
+				AddressStreet = string.Format("{0}\n{1}\n{2}\n{3}",
+					jobject.GetValue("street_address_1")?.ToObject<string?>(),
+					jobject.GetValue("street_address_2")?.ToObject<string?>(),
+					jobject.GetValue("street_address_3")?.ToObject<string?>(),
+					jobject.GetValue("street_address_4")?.ToObject<string?>()),
+				Category = jobject.GetValue("category")?.ToObject<string?>(),
+				GeoCode = jobject.GetValue("geo_code")?.ToObject<string?>(),
+				GeoLevel = jobject.GetValue("geo_level")?.ToObject<string?>(),
+				IsDisestablished = jobject.GetValue("is_disestablished")?.ToObject<bool?>(),
+				MiifCategory = jobject.GetValue("miif_category")?.ToObject<string?>(),
+				Name = jobject.GetValue("name")?.ToObject<string?>(),
+				NameLong = jobject.GetValue("long_name")?.ToObject<string?>(),
+				NumberFax = jobject.GetValue("fax_number")?.ToObject<string?>(),
+				NumberPhone = jobject.GetValue("phone_number")?.ToObject<string?>(),
+				Population = jobject.GetValue("population")?.ToObject<int?>(),
+				SquareKms = jobject.GetValue("square_kms")?.ToObject<decimal?>(),
+				PkCountry = country?.Pk,
+				PkDistrict = district?.Pk,
+				PkProvince = province?.Pk,
+				UrlWebsite = jobject.GetValue("url")?.ToObject<string?>(),
+			};
+		}
+		public static Municipality MuniciplityFromMembers(this JObject jobject, Params? _params)
+		{
+			Province? province = _params?.ConnectionProvinces?
+				.TableEnumerable<Province>()
+				.FirstOrDefault(_ =>
+				{
+					return
+						string.Equals(_.Code, jobject.GetValue("municipality.province_code")?.ToObject<string?>(), StringComparison.OrdinalIgnoreCase) ||
+						string.Equals(_.Name, jobject.GetValue("municipality.province_name")?.ToObject<string?>(), StringComparison.OrdinalIgnoreCase);
+				});
 
-			if (jobject.TryGetValue(Province.SQL.Column_Population, StringComparison.OrdinalIgnoreCase, out JToken? population) && population?.ToObject<int?>() is int _population)
-				province.Population = _population;
-			else logger.WriteLine("Population");
+			return new Municipality
+			{
+				AddressPostal = string.Format("{0}\n{1}\n{2}",
+					jobject.GetValue("municipality.postal_address_1")?.ToObject<string?>(),
+					jobject.GetValue("municipality.postal_address_2")?.ToObject<string?>(),
+					jobject.GetValue("municipality.postal_address_3")?.ToObject<string?>()),
+				AddressStreet = string.Format("{0}\n{1}\n{2}\n{3}",
+					jobject.GetValue("municipality.street_address_1")?.ToObject<string?>(),
+					jobject.GetValue("municipality.street_address_2")?.ToObject<string?>(),
+					jobject.GetValue("municipality.street_address_3")?.ToObject<string?>(),
+					jobject.GetValue("municipality.street_address_4")?.ToObject<string?>()),
+				Category = jobject.GetValue("municipality.category")?.ToObject<string?>(),
+				GeoCode = jobject.GetValue("municipality.demarcation_code")?.ToObject<string?>(),
+				MiifCategory = jobject.GetValue("municipality.miif_category")?.ToObject<string?>(),
+				Name = jobject.GetValue("municipality.name")?.ToObject<string?>(),
+				NameLong = jobject.GetValue("municipality.long_name")?.ToObject<string?>(),
+				NumberFax = jobject.GetValue("municipality.fax_number")?.ToObject<string?>(),
+				NumberPhone = jobject.GetValue("municipality.phone_number")?.ToObject<string?>(),
+				PkProvince = province?.Pk,
+				UrlWebsite = jobject.GetValue("municipality.url")?.ToObject<string?>(),
+			};
+		}
+		public static Province ProvinceFromDefault(this JObject jobject, Params? _params)
+		{
+			Country? country = _params?.ConnectionCountries?
+				.Table<Country>()
+				.FirstOrDefault(_ => _.Code == Country.Codes.SouthAfrica);
 
-			if (jobject.TryGetValue(Province.SQL.Column_SquareKms, StringComparison.OrdinalIgnoreCase, out JToken? squarekms) && squarekms?.ToObject<decimal?>() is decimal _squarekms)
-				province.SquareKms = _squarekms;
-			else logger.WriteLine("SquareKms");
-
-			if (jobject.TryGetValue(Province.SQL.Column_UrlCoatOfArms, StringComparison.OrdinalIgnoreCase, out JToken? urlcoatofarms) && urlcoatofarms?.ToObject<string?>() is string _urlcoatofarms)
-				province.UrlCoatOfArms = _urlcoatofarms;
-			else logger.WriteLine("UrlCoatOfArms");
-
-			if (jobject.TryGetValue(Province.SQL.Column_UrlWebsite, StringComparison.OrdinalIgnoreCase, out JToken? urlwebsite) && urlwebsite?.ToObject<string?>() is string _urlwebsite)
-				province.UrlWebsite = _urlwebsite;
-			else logger.WriteLine("UrlWebsite");
-
-			return province;
+			return new Province
+			{
+				Capital = jobject.GetValue(Province.SQL.Column_Capital, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				Code = jobject.GetValue(Province.SQL.Column_Code, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				Name = jobject.GetValue(Province.SQL.Column_Name, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				Population = jobject.GetValue(Province.SQL.Column_Population, StringComparison.OrdinalIgnoreCase)?.ToObject<int?>(),
+				SquareKms = jobject.GetValue(Province.SQL.Column_SquareKms, StringComparison.OrdinalIgnoreCase)?.ToObject<decimal?>(),
+				UrlCoatOfArms = jobject.GetValue(Province.SQL.Column_UrlCoatOfArms, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+				UrlWebsite = jobject.GetValue(Province.SQL.Column_UrlWebsite, StringComparison.OrdinalIgnoreCase)?.ToObject<string?>(),
+			};
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using Newtonsoft.Json.Linq;
 
 using SQLite;
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 using XycloneDesigns.Apis.General.Tables;
 
 namespace Database.General
@@ -24,49 +29,72 @@ namespace Database.General
 			StreamWriters streamwriters = [];
 
 			FileInfo fileinfo_countries = new (Path.Combine(DirectoryOutputs, "countries.db"));
+			FileInfo fileinfo_district = new (Path.Combine(DirectoryOutputs, "districts.db"));
 			FileInfo fileinfo_languages = new (Path.Combine(DirectoryOutputs, "languages.db"));
 			FileInfo fileinfo_municipalities = new (Path.Combine(DirectoryOutputs, "municipalities.db"));
 			FileInfo fileinfo_provinces = new (Path.Combine(DirectoryOutputs, "provinces.db"));
 
 			SQLiteConnection sqliteconnection_countries = new (fileinfo_countries.FullName); 
+			SQLiteConnection sqliteconnection_districts = new (fileinfo_district.FullName); 
 			SQLiteConnection sqliteconnection_languages = new (fileinfo_languages.FullName); 
 			SQLiteConnection sqliteconnection_municipalities = new (fileinfo_municipalities.FullName); 
 			SQLiteConnection sqliteconnection_provinces = new (fileinfo_provinces.FullName);
 
 			sqliteconnection_countries.CreateTable<Country>();
+			sqliteconnection_districts.CreateTable<District>();
 			sqliteconnection_languages.CreateTable<Language>();
 			sqliteconnection_municipalities.CreateTable<Municipality>();
 			sqliteconnection_provinces.CreateTable<Province>();
 
-			Process<Country>(sqliteconnection_countries, "countries", streamwriters);
-			Process<Language>(sqliteconnection_languages, "languages", streamwriters);
-			Process<Municipality>(sqliteconnection_municipalities, "municipalities", streamwriters);
-			Process<Province>(sqliteconnection_provinces, "provinces", streamwriters);
+			JObjectExtensions.Params _params = new()
+			{
+				ConnectionCountries = sqliteconnection_countries,
+				ConnectionDistricts = sqliteconnection_districts,
+				ConnectionLanguages = sqliteconnection_languages,
+				ConnectionMunicipalities = sqliteconnection_municipalities,
+				ConnectionProvinces = sqliteconnection_provinces,
+			};
+
+			sqliteconnection_languages
+				.InsertAll(Process_LanguagesDefault(_params), out int _)
+				.Commit();
+
+			sqliteconnection_countries
+				.InsertAll(Process_CountriesDefault(_params), out int _)
+				.Commit();
+
+			sqliteconnection_provinces
+				.InsertAll(Process_ProvincesDefault(_params), out int _)
+				.Commit();
+
+			sqliteconnection_districts
+				.InsertAll(Process_DistrictsMunicipalMoneyGeography(_params).DistinctBy(_ => _.Code), out int _)
+				.Commit();
+
+			sqliteconnection_municipalities
+				.InsertAll(Process_MunicipalitesMunicipalMoneyGeography(_params), out int _)
+				//.InsertAll(Process_MunicipalitesMunicipalMoneyMembers(_params), out int _)
+				.Commit();
 
 			sqliteconnection_countries.CommitAndClose();
+			sqliteconnection_districts.CommitAndClose();
 			sqliteconnection_languages.CommitAndClose();
 			sqliteconnection_municipalities.CommitAndClose();
 			sqliteconnection_provinces.CommitAndClose();
 
-			string countrieszipfile = fileinfo_countries.ZipFile();
-			string countriesgzipfile = fileinfo_countries.GZipFile();
-			string languageszipfile = fileinfo_languages.ZipFile();
-			string languagesgzipfile = fileinfo_languages.GZipFile();
-			string municipalitieszipfile = fileinfo_municipalities.ZipFile();
-			string municipalitiesgzipfile = fileinfo_municipalities.GZipFile();
-			string provinceszipfile = fileinfo_provinces.ZipFile();
-			string provincesgzipfile = fileinfo_provinces.GZipFile();
-
-			apifiles.Add(countrieszipfile, "countries");
-			apifiles.Add(countriesgzipfile, "countries");
-			apifiles.Add(languageszipfile, "languages");
-			apifiles.Add(languagesgzipfile, "languages");
-			apifiles.Add(municipalitieszipfile, "municipalities");
-			apifiles.Add(municipalitiesgzipfile, "municipalities");
-			apifiles.Add(provinceszipfile, "provinces");
-			apifiles.Add(provincesgzipfile, "provinces");
+			apifiles.Add(fileinfo_countries.ZipFile(), "countries");
+			apifiles.Add(fileinfo_countries.GZipFile(), "countries");
+			apifiles.Add(fileinfo_district.ZipFile(), "district");
+			apifiles.Add(fileinfo_district.GZipFile(), "district");
+			apifiles.Add(fileinfo_languages.ZipFile(), "languages");
+			apifiles.Add(fileinfo_languages.GZipFile(), "languages");
+			apifiles.Add(fileinfo_municipalities.ZipFile(), "municipalities");
+			apifiles.Add(fileinfo_municipalities.GZipFile(), "municipalities");
+			apifiles.Add(fileinfo_provinces.ZipFile(), "provinces");
+			apifiles.Add(fileinfo_provinces.GZipFile(), "provinces");
 
 			fileinfo_countries.Delete();
+			fileinfo_district.Delete();
 			fileinfo_languages.Delete();
 			fileinfo_municipalities.Delete();
 			fileinfo_provinces.Delete();
@@ -98,41 +126,88 @@ namespace Database.General
 		{
 			Console.WriteLine("Cleaning Up...");
 		}
-		static void Process<TTable>(SQLiteConnection sqliteconnection, string name, StreamWriters streamwriters) where TTable : _Table
+
+		static IEnumerable<Country> Process_CountriesDefault(JObjectExtensions.Params _params)
 		{
-			string inputpath = Path.Combine(DirectoryInputs, name);
-			string outputpath = Path.Combine(DirectoryOutputs, name);
+			string directory = Path.Combine(DirectoryInputs, "countries");
 
-			Directory.CreateDirectory(streamwriters.PathBase = outputpath);
-
-			Func<JObject, StreamWriter, object> table = (jobject, streamwriter) => true switch
+			foreach (string filepath in Directory.EnumerateFiles(directory))
 			{
-				true when typeof(TTable) == typeof(Country) => jobject.ToCountry(streamwriter),
-				true when typeof(TTable) == typeof(Language) => jobject.ToLanguage(streamwriter),
-				true when typeof(TTable) == typeof(Municipality) => jobject.ToMunicipality(streamwriter),
-				true when typeof(TTable) == typeof(Province) => jobject.ToProvince(streamwriter),
-				
-				_ => throw new ArgumentException(),
-			};
+				using FileStream filestream = File.OpenRead(filepath);
+				using StreamReader streamreader = new(filestream);
 
-			List<object> objects = Directory.EnumerateFiles(inputpath)
-				.Select(file =>
-				{
-					streamwriters.Add(file, string.Format("{0}.txt", file.Split('\\')[^1]), true);
+				string json = streamreader.ReadToEnd();
 
-					using FileStream filestream = File.OpenRead(file);
-					using StreamReader streamreader = new(filestream);
+				yield return JObject
+					.Parse(json)
+					.CountryFromDefault(_params);
+			}
+		}
+		static IEnumerable<District> Process_DistrictsMunicipalMoneyGeography(JObjectExtensions.Params _params)
+		{
+			string path = Path.Combine(DirectoryInputs, "municipalities", "municipalmoney.municipalities.geography.json");
+			using FileStream filestream = File.OpenRead(path);
+			using StreamReader streamreader = new(filestream);
 
-					string json = streamreader.ReadToEnd();
-					object insert = table.Invoke(JObject.Parse(json), streamwriters[file]);
+			string json = streamreader.ReadToEnd();
 
-					streamwriters.Dispose(true, file);
+			foreach (JToken jtoken in JArray.Parse(json))
+				if (((JObject)jtoken).DistrictFromGeography(_params) is District district)
+					yield return district;
+		}
+		static IEnumerable<Language> Process_LanguagesDefault(JObjectExtensions.Params _params)
+		{
+			string directory = Path.Combine(DirectoryInputs, "languages");
 
-					return insert;
+			foreach (string filepath in Directory.EnumerateFiles(directory))
+			{
+				using FileStream filestream = File.OpenRead(filepath);
+				using StreamReader streamreader = new(filestream);
 
-				}).ToList();
+				string json = streamreader.ReadToEnd();
 
-			sqliteconnection.InsertAll(objects);
+				yield return JObject
+					.Parse(json)
+					.LanguageFromDefault(_params);
+			}
+		}
+		static IEnumerable<Municipality> Process_MunicipalitesMunicipalMoneyGeography(JObjectExtensions.Params _params)
+		{
+			string path = Path.Combine(DirectoryInputs, "municipalities", "municipalmoney.municipalities.geography.json");
+			using FileStream filestream = File.OpenRead(path);
+			using StreamReader streamreader = new(filestream);
+
+			string json = streamreader.ReadToEnd();
+
+			foreach (JToken jtoken in JArray.Parse(json))
+				yield return ((JObject)jtoken).MuniciplityFromGeography(_params);
+		}
+		static IEnumerable<Municipality> Process_MunicipalitesMunicipalMoneyMembers(JObjectExtensions.Params _params)
+		{
+			string path = Path.Combine(DirectoryInputs, "municipalities", "municipalmoney.municipalities.members.json");
+			using FileStream filestream = File.OpenRead(path);
+			using StreamReader streamreader = new(filestream);
+
+			string json = streamreader.ReadToEnd();
+
+			foreach (JToken jtoken in (JArray)JObject.Parse(json).GetValue("data")!)
+				yield return ((JObject)jtoken).MuniciplityFromMembers(_params);
+		}
+		static IEnumerable<Province> Process_ProvincesDefault(JObjectExtensions.Params _params)
+		{
+			string directory = Path.Combine(DirectoryInputs, "provinces");
+
+			foreach (string filepath in Directory.EnumerateFiles(directory))
+			{
+				using FileStream filestream = File.OpenRead(filepath);
+				using StreamReader streamreader = new(filestream);
+
+				string json = streamreader.ReadToEnd();
+
+				yield return JObject
+					.Parse(json)
+					.ProvinceFromDefault(_params);
+			}
 		}
 	}
 
@@ -154,7 +229,7 @@ namespace Database.General
 				{ "DateEdited", DateTime.Now.ToString("dd-MM-yyyy") },
 				{ "Name", filename.Split('/').Last() },
 				{ "Url", string.Format("https://raw.githubusercontent.com/xyclone-designs/database.general/refs/heads/main/.output/{0}", filename) },
-				{ "Description", string.Format("individual {0}database for {1}", description, type) }
+				{ "Description", string.Format("indivcodeual {0}database for {1}", description, type) }
 			});
 		}
 	}
